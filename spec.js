858beedcb389184d4e9d9036ed4edd7b77cdc672
Oleh <odziu@softserveinc.com>
2019-02-24 14:46:59 +0200

    49 minutes

--- spec.js
+++ spec.js
@@ -16,11 +16,39 @@ describe('browserName', function () {
         console.time('sleep')
         await browser.sleep(5000)
         console.timeEnd('sleep')
-	});
+    });
+    
+    it('can fork new driver instance', async function () {
+        //await browser.waitForAngularEnabled(false)
+        await browser.get('http://movies-finder.firebaseapp.com/')
+
+        let browser2 = await browser.forkNewDriverInstance().ready;
+        await browser2.get('https://www.protractortest.org/#/')
+        await browser2.sleep(5000)
+        await browser2.close()
+        // Direct search in specified browser
+        await browser2.$('div')
+    });
+    
+    fit('can switch to iframe', async () => {
+        await browser.waitForAngularEnabled(false)
+        await browser.get('http://the-internet.herokuapp.com/iframe')
+        let iframe = $('#mce_0_ifr')
+        // BUG - test stops when passing ElementFinder into .frame()
+        // await browser.switchTo().frame(iframe)
+        await browser.switchTo().frame(iframe.getWebElement())
+        //console.log(await $('body#tinymce').getText())
+
+        // Should check text
+        //let tinymce = await element.$('body#tinymce');
+        //expect(await tinymce.getText().toEqual('Your content goes here.'))
+        // expect(await element.$('body#tinymce').getText().toEqual('Your content goes here.'))
+        await browser.switchTo().defaultContent()
+    });
 });
 
 describe('Protractor searching', () => {
-    fit('should calculate the sum of two numbers', async () => {
+    it('should calculate the sum of two numbers', async () => {
         await browser.get('http://movies-finder.firebaseapp.com/');
  	});
 });
