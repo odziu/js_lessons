f2c809e61576fc0ee5d24bfe7c29b6e71ee016ed
odziu <dzyubak12@gmail.com>
2019-06-04 22:44:28 +0300

    2 realisations

--- specs/additional_lessons_specs/quadratic_equation_spec.js
+++ specs/additional_lessons_specs/quadratic_equation_spec.js
@@ -1,26 +1,32 @@
-let a;
-let b;
-let c;
-
+let a = prompt('a =', '');
+let b = prompt('b =', '');
+let c = prompt('c =', '');
 let d = Math.pow(b, 2) - 4 * a * c;
+let x1 = (-b + Math.sqrt(d)) / (2 * a);
+let x2 = (-b - Math.sqrt(d)) / (2 * a);
+let x = -b / (2 * a);
 
-let x;
-let x1;
-let x2;
+let answer = (d > 0) ? 'x1 = ' + x1 + '; x2 = ' + x2 :
+  (d == 0) ? 'x = ' + x :
+  'Discriminant: D = ' + d +'; D < 0';
+alert(answer);
 
-d > 0;
-x1 = (-b + Math.sqrt(d)) / (2 * a);
-x2 = (-b - Math.sqrt(d)) / (2 * a);
+// let a = prompt('a =', '');
+// let b = prompt('b =', '');
+// let c = prompt('c =', '');
+// let d = Math.pow(b, 2) - 4 * a * c;
 
-d = 0;
-x = -b / (2 * a);
+// if(d > 0) {
+//     let x1 = (-b + Math.sqrt(d)) / (2 * a);
+//     let x2 = (-b - Math.sqrt(d)) / (2 * a);
+//     alert('x1 = ' + x1 + '; x2 = ' + x2);
+// } else if (d == 0) {
+//     let x = -b / (2 * a);
+//     alert('x = ' + x);
+// } else {
+//     alert('Discriminant: D = ' + d +'; D < 0');
+// };
 
-if(d > 0) {
-    alert('x1 = ' + x1);
-    alert('x2 = ' + x2);
-} else {
-    alert('x = ' + x);
-} else {
-    alert('fuck you!')
-}
+
+
 
